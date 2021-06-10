5e348f103aed050bcb625fd8b78191e7269ea4e8
Ariane <pikabooariane@hotmail.fr>
2021-06-10 12:57:46 +0300

    added index route for cities

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:update, :show]
   get 'profile', to: 'users#profile'
+
+  resources :cities, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
